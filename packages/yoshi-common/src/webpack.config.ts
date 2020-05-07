5572ed7d0501dff6e405741a5903b9a4f37372d5
Ronen Amiel <ronen.amiel@gmail.com>
2020-05-07 13:29:08 +0300

    add a unique chunkCallbackName according to package name

--- packages/yoshi-common/src/webpack.config.ts
+++ packages/yoshi-common/src/webpack.config.ts
@@ -480,6 +480,10 @@ export function createBaseWebpackConfig({
 
       // https://github.com/wix/yoshi/pull/497
       jsonpFunction: `webpackJsonp_${toIdentifier(name)}`,
+
+      // @ts-ignore Not available in @types/webpack
+      // https://webpack.js.org/configuration/output/#outputchunkcallbackname
+      chunkCallbackName: `webpackWorker_${utils_1.toIdentifier(name)}`,
     },
 
     resolve: {

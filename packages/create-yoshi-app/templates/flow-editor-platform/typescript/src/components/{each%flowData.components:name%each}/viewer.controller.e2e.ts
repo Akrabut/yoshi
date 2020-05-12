4ce1284c974280cf42a3e55a5b382f47a973a76d
Artem Yavorsky <aqson@me.com>
2020-05-12 14:42:39 +0300

    Update controller test to fix race condition

--- packages/create-yoshi-app/templates/flow-editor-platform/typescript/src/components/{each%flowData.components:name%each}/viewer.controller.e2e.ts
+++ packages/create-yoshi-app/templates/flow-editor-platform/typescript/src/components/{each%flowData.components:name%each}/viewer.controller.e2e.ts
@@ -13,10 +13,22 @@ describe('Viewer App', () => {
 
     await page.click('button');
 
-    const widgetTextAfterClick = await page.$eval('h2', node =>
-      node?.textContent?.toLowerCase(),
-    );
+    const targetText = 'You clicked the button';
 
-    expect(widgetTextAfterClick).toEqual('you clicked the button');
+    try {
+      await page.waitForFunction(
+        (text: string) =>
+          document?.querySelector('h2')?.innerText.includes(text),
+        {},
+        targetText,
+      );
+    } catch (e) {
+      const actualWidgetText = await page.$eval('h2', node =>
+        node?.textContent?.toLowerCase(),
+      );
+      throw new Error(
+        `The text "${targetText}" was not found on the page. "${actualWidgetText}" found instead`,
+      );
+    }
   });
 });
